57e34276ef45ae1309448bbc641348bd79f42d30
Anna Telisov <annatelisov@gmail.com>
2024-08-20 19:29:49 +0300

    updates

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const server = http.createServer(app);
 
 app.use(cors({
   //Allow my frontend, http://localhost:3000 for localhost
-  origin: 'https://frontendcodeblocksapp-d21f3e724ed4.herokuapp.com', 
+  //origin: 'https://frontendcodeblocksapp-d21f3e724ed4.herokuapp.com', 
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
@@ -16,61 +17,70 @@ app.use(cors({
 const io = socketIo(server, {
   cors: {
     //Allow my frontend, http://localhost:3000 for localhost
-    origin: 'https://frontendcodeblocksapp-d21f3e724ed4.herokuapp.com', 
+   // origin: 'https://frontendcodeblocksapp-d21f3e724ed4.herokuapp.com', 
+    origin: 'http://localhost:3000', 
     methods: ['GET', 'POST']
   }
 });
 
 app.use(express.json());
 
-// MongoDB connection
+//MongoDB connection
   mongoose.connect('mongodb+srv://annatelisov:A311016o@codeblocks-cluster.ovvx9.mongodb.net/codeblocks?retryWrites=true&w=majority', {})  
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 
-// API routes
+//API routes
 app.use('/api/codeblocks', require('./routes/codeblocks'));
 
-var peoples;
-var role;
-
-// Socket.IO for real-time code updates
+//Socket.IO for real-time code updates
 io.on('connection', (socket) => {
-  socket.on('joinBlock', (blockId) => {
-    //Check if someone already inside this room(codeblock)
-    const room = io.sockets.adapter.rooms.get(blockId);
-    //if not initialize 0 people
-    peoples = room ? room.size : 0;
-    //if this is the first person this is the mentor, if there is already people inside do this is a student
-    if (peoples === 0) {
-        role = 'mentor';
-    } else {
-        role = 'student';
-    }
-    socket.emit('roleAssignment', role);
-    console.log(`User joined block ${blockId}. Role assigned: ${role}. Number of users: ${peoples}`);
+  let currentRole;
 
-    //join person to the room
+  socket.on('joinBlock', (blockId) => {
     socket.join(blockId);
-    //update count that 1 more student is in
-    peoples = peoples + 1;
-    if(role === 'student'){
-      io.to(blockId).emit('studentsCount', peoples);
+
+    //Check the number of people in the room
+    const room = io.sockets.adapter.rooms.get(blockId);
+    const numberOfUsers = room ? room.size : 0;
+
+    //Define roles based on this if this first person in room or not
+    if (numberOfUsers === 1) {
+      currentRole = 'mentor';
+    } else {
+      currentRole = 'student';
     }
-});
+
+    socket.emit('roleAssignment', currentRole);
+
+    //Update the student number for all users in this room without the mentor
+    if (currentRole === 'student') {
+      io.to(blockId).emit('studentsCount', numberOfUsers - 1); 
+    }
+  });
 
   socket.on('codeChange', ({ blockId, code }) => {
     io.to(blockId).emit('updateCode', code);
   });
 
   socket.on('leaveBlock', (blockId) => {
-    console.log('Someone disconnected');
-    //update count that 1 less student is inside
-    peoples = peoples - 1;
-    io.to(blockId).emit('studentsCount', peoples);
+    console.log("disconnect");
+    //Update the number of people in the room after a student leaves
+    const room = io.sockets.adapter.rooms.get(blockId);
+    const numberOfUsers = room ? room.size : 0;
+
+    if (currentRole === 'mentor') {
+      //If the mentor leaves, close the room for everyone
+      io.to(blockId).emit('roomClosed');
+      io.socketsLeave(blockId);
+    } else {
+      //If student leaves, update number of students in room
+      io.to(blockId).emit('studentsCount', numberOfUsers - 2); 
+    }
   });
 });
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
